0eb07f0c9fd5c8bf2f74cdbf18112baa1f99f1dc
Jenny Shamoo <jenny.shamoo@gmail.com>
2020-03-10 22:09:48 -0700

    complete maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const counter = {};
+  for (let char of str) {
+    if (!counter[char]) {
+      counter[char] = 1;
+    } else {
+      counter[char]++;
+    }
+  }
+  let mostCommon = str[0];
+  for (let key in counter) {
+    if (counter[key] > counter[mostCommon]) {
+      mostCommon = key;
+    }
+  }
+  return mostCommon;
+}
 
 module.exports = maxChar;
