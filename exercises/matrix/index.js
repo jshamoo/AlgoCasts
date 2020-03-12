23f3b7c0940eeabe23dd207561185e30079ee7d4
Jenny Shamoo <jenny.shamoo@gmail.com>
2020-03-12 13:48:23 -0700

    complete matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,46 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const cube = [];
+  for (let i = 0; i < n; i++) {
+    cube.push(Array(n));
+  }
+  let start = 0;
+  let end = n - 1;
+  let num = 1;
+  while (num <= n * n) {
+    let col = start;
+    let row = start;
+    while(col <= end){
+      cube[row][col] = cube[row][col] || num++;
+      if (col === end) row++;
+      col++;
+    }
+    col--;
+    while(row <= end) {
+      cube[row][col] = cube[row][col] || num++;
+      if(row === end) col--;
+      row++;
+    }
+    row--;
+    while(col >= start) {
+      cube[row][col] = cube[row][col] || num++;
+      if (col === start) row--;
+      col--;
+    }
+    col++;
+    while (row >= start) {
+      cube[row][col] = cube[row][col] || num++;
+      if(row === start) col++;
+      row--;
+    }
+    start = start + 1;
+    end = end - 1;
+  }
+  return cube;
+}
+
+console.log(matrix(4))
 
 module.exports = matrix;
