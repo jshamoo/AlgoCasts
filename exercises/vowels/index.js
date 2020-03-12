0f440469690a5c662c4397efcbbc4478bc36f3c4
Jenny Shamoo <jenny.shamoo@gmail.com>
2020-03-12 12:52:23 -0700

    add solution using match method

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -8,14 +8,19 @@
 //   vowels('Why?') --> 0
 
 function vowels(str) {
-  const vowels = ['a', 'e', 'i', 'o', 'u'];
-  let count = 0;
-  for (let char of str.toLowerCase()) {
-    if (vowels.includes(char)) {
-      count++;
-    }
-  }
-  return count;
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
 }
 
 module.exports = vowels;
+
+// function vowels(str) {
+//   const vowels = ['a', 'e', 'i', 'o', 'u'];
+//   let count = 0;
+//   for (let char of str.toLowerCase()) {
+//     if (vowels.includes(char)) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
