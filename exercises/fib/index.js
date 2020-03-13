de45a43e4ff227866bcf87f02fa211d8cc065850
Jenny Shamoo <jenny.shamoo@gmail.com>
2020-03-13 11:17:59 -0700

    complete fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,35 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n) {
+//   let entries = [0, 1];
+//   for (let i = 2; i <= n; i++) {
+//     entries[i] = entries[i - 1] + entries[i - 2];
+//   }
+//   return entries[n];
+// }
+
+// recursive - exponential time complexity (2^n)
+function slowFib(n) {
+  if (n < 2) {
+    return n;
+  }
+  return slowFib(n - 1) + slowFib(n - 2);
+}
+
+// we can use memoize to make it faster by reducing repeated func calling
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  }
+}
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
