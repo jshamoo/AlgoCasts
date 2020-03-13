564b21f7235f4cf890bc098ac8104b2402c83890
Jenny Shamoo <jenny.shamoo@gmail.com>
2020-03-13 14:30:48 -0700

    complete qfroms

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,33 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.stackA = new Stack();
+    this.stackB = new Stack();
+  }
+
+  add(v) {
+    while (this.stackB.peek()) {
+      this.stackA.push(this.stackB.pop())
+    }
+    this.stackA.push(v);
+  }
+
+  peek() {
+    while (this.stackA.peek()) {
+      this.stackB.push(this.stackA.pop())
+    }
+    return this.stackB.peek();
+  }
+
+  remove() {
+    while (this.stackA.peek()) {
+      this.stackB.push(this.stackA.pop())
+    }
+    return this.stackB.pop();
+  }
+
+}
 
 module.exports = Queue;
